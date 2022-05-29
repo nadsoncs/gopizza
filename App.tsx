f4b6d090f3a5a40abd060af50373c5230b8d1565
Nadson Cerqueira <nadsoncerqueira@gmail.com>
2022-05-29 16:31:51 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import * as SplashScreen from 'expo-splash-screen';
+import { StatusBar } from 'expo-status-bar';
+
+import { useFonts, DMSans_400Regular } from '@expo-google-fonts/dm-sans';
+import { DMSerifDisplay_400Regular} from '@expo-google-fonts/dm-serif-display';
+
+import { ThemeProvider } from 'styled-components/native';
+import { AuthProvider } from '@hooks/auth';
+
+import theme from './src/theme';
+import { SignIn } from "@screens/SignIn"
+export default function App() {
+  SplashScreen.preventAutoHideAsync();
+  const [fontsLoaded] = useFonts({
+    DMSans_400Regular,
+    DMSerifDisplay_400Regular
+  });
+
+  if(!fontsLoaded) {
+    return null;
+  }
+  SplashScreen.hideAsync();
+
+  return (
+    <ThemeProvider theme={theme}>
+      <StatusBar 
+        style="light" 
+        translucent
+        backgroundColor="transparent"
+      />
+      <AuthProvider>
+        <SignIn />
+      </AuthProvider>
+    </ThemeProvider>
+  );
+}
